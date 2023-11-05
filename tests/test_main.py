3d44b02fa069d8ff45a67de2acc073a8fff98669
Nanda Gopal Pattanayak <40311776+NandaGopal56@users.noreply.github.com>
2023-11-05 19:08:48 +0530

    test setup

---
+++ tests/test_main.py
@@ -0,0 +1,35 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+from main import app
+from database.base import get_db, Base
+from fastapi.testclient import TestClient
+import pytest
+
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///./test_sqllite.db"
+# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
+
+test_engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+
+TestSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=test_engine)
+
+# Dependency to get the SQLAlchemy session
+def override_test_get_db():
+    db = TestSessionLocal()
+    try:
+        yield db
+    finally:
+        db.close() 
+
+app.dependency_overrides[get_db] = override_test_get_db
+
+testClient = TestClient(app)
+
+
+def test_main_home():
+    response = testClient.get("/")
+    assert response.status_code == 200
+    assert response.json() == 'Welcome to the Notes App'
+
--- tests/test_notes.py
+++ tests/test_notes.py
@@ -1,10 +1,31 @@
-from fastapi.testclient import TestClient
-from main import app
+from .test_main import testClient, test_engine
+from database.base import  Base
 
+def setup() -> None:
+    # Create the tables in the test database
+    Base.metadata.create_all(bind=test_engine)
 
-client = TestClient(app)
+def teardown() -> None:
+    # Drop the tables in the test database
+    Base.metadata.drop_all(bind=test_engine)
 
-def test_main_home():
-    response = client.get("/")
+###############################################
+#           TESTS
+###############################################
+
+def test_create_note():
+    response = testClient.post(
+        "/notes/", json={
+                "title": "pytest note title",
+                "content": "pytest note Content"
+            }
+    )
     assert response.status_code == 200
-    assert response.json() == 'Welcome to the Notes App'
+    data = response.json() 
+    assert data['title'] == 'pytest note title'
+    assert data['content'] == 'pytest note Content'
+
+
+
+
+
