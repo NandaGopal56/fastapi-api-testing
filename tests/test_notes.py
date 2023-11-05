267cf80812c8825eb6562849662a7504125a460d
Nanda Gopal Pattanayak <40311776+NandaGopal56@users.noreply.github.com>
2023-11-05 19:15:48 +0530

    tests for CRUD endpoints

--- tests/test_notes.py
+++ tests/test_notes.py
@@ -25,7 +25,30 @@ def test_create_note():
     assert data['title'] == 'pytest note title'
     assert data['content'] == 'pytest note Content'
 
+def test_get_all_notes():
+    response = testClient.get("/notes/all")
+    assert response.status_code == 200
 
+def test_get_note():
+    response = testClient.get("/notes/1")
+    assert response.status_code == 200
+    data = response.json() 
+    assert data['id'] == 1
+    assert data['title'] == 'pytest note title'
+    assert data['content'] == 'pytest note Content'
 
+def update_get_note():
+    response = testClient.put("/notes/1", json={
+                "title": "pytest note title updated",
+                "content": "pytest note Content updated"
+            })
+    assert response.status_code == 200
+    data = response.json()
+    assert data['title'] == 'pytest note title updated'
+    assert data['content'] == 'pytest note Content updated'
 
-
+def delete_a_note():
+    response = testClient.delete("/notes/1")
+    assert response.status_code == 200
+    data = response.json()
+    assert data['message'] == 'Note deleted successfully'
